b3fd4a1964e9769864944ffe97a77093eb4c270f
KrishnaBagade <96902596+KrishnaBagade@users.noreply.github.com>
2024-01-29 12:45:04 +0000

    Corrected the code which was giving incorrect number,symbol,letters count which was given by user.

--- Day5/Pypassword_Generator.py
+++ Day5/Pypassword_Generator.py
@@ -1,5 +1,6 @@
 #Password Generator Project
 import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -7,32 +8,17 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-password=""
 character_list=[letters,numbers,symbols]
 run_count=nr_letters+nr_symbols+nr_numbers
-print(run_count)
-while True:
-  random_character=random.randint(0,2)
-  letter_count=0
-  symbol_count=0
-  number_count=0
-  if random_character == 0:
-    letter_count=+1
-    if letter_count<=nr_letters:
-     j=random.randint(0,51)
-     password+=letters[j]
-  if random_character == 1:
-    symbol_count+=1
-    if symbol_count<=nr_symbols:
-      g=random.randint(0,8)
-      password+=symbols[g]
-  if random_character == 2:
-     number_count+=1
-     if number_count<=nr_numbers:
-       h=random.randint(0,9)
-       password+=numbers[h]
-  p=len(password)
-  if p == run_count:
-    break
-print(f"Your password is:{password}")
-      
+password =[]
+for count in range(nr_letters):
+  letter = letters[random.randint(0,count)]
+  password.append(letter)
+for count in range(nr_numbers):
+  letter = numbers[random.randint(0,count)]
+  password.append(letter)
+for count in range(nr_symbols):
+  letter = symbols[random.randint(0,count)]
+  password.append(letter)
+random.shuffle(password)
+print(''.join(password))
