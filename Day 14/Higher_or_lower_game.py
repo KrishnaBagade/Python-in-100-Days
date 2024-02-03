4ea1d05d337dab4eece68317cb83d55d835a72e6
KrishnaBagade <96902596+KrishnaBagade@users.noreply.github.com>
2024-02-03 16:55:29 +0000

    created high or low game and made a read.md

---
+++ Day 14/Higher_or_lower_game.py
@@ -0,0 +1,45 @@
+from random import randint
+
+from art import logo, vs
+from game_data import data
+from replit import clear
+
+
+def compare(user_choice,data,a,b,score):
+  fcv =data[a]['follower_count']
+  fcb =data[b]['follower_count']
+  if (fcv>fcb and user_choice=="a") or (fcv<fcb and user_choice=="b"):
+    clear()
+    score+=1
+    print(f"You guessed it right. Current score: {score}")
+    a=randint(0,49)
+    b=randint(0,49)
+    while a==b:
+     a=randint(0,49)
+     b=randint(0,49)
+    game(score,a,b)
+  else:
+    clear()
+    print(f"Sorry, that's wrong. Final score: {score}")
+
+def game(score,a,b):
+  print(logo)
+  print(f"Compare A: {data[a]['name']}, a {data[a]['description']}, from {data[a]['country']}")
+  print(vs)
+  print(f"Against B: {data[b]['name']}, a {data[b]['description']}, from {data[b]['country']}")
+  while True:
+    user_choice=input("Who has more followers? Type 'A' or 'B', E:Exit ").lower()
+    if user_choice != "a" and user_choice != "b" :
+      print("Please enter either a or b only.")
+    elif user_choice == 'e':
+      break
+    else:
+      compare(user_choice,data,a,b,score)
+  
+score=0
+a=0
+b=0
+while a==b:
+  a=randint(0,49)
+  b=randint(0,49)
+game(score,a,b)
